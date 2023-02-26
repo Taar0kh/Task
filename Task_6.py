8eb4d79aa3892b9ef93daae9def3ba7ac5f23d55
Taar0kh <126300935+Taar0kh@users.noreply.github.com>
2023-02-26 13:13:51 +0100

    Update Task_6.py

--- Task_6.py
+++ Task_6.py
@@ -1,4 +1,4 @@
 !pip install CoolProp
 import CoolProp.CoolProp as CP
 Sat_Temp = CP.PropsSI('T','P',20*100000,'Q',1,'Water')
-print("saturated temperature for water at 20 bar is " + str(Sat_Temp) + " Kelvin"
+print("saturated temperature of water in 20 bar is " + str(round(Sat_Temp,3)) + " Kelvin")
